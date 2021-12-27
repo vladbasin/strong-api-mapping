90af153e5a2a72bdad3ba2cbbd01fcf7f94740b7
Vlad Basin <vladbasin@gmail.com>
2021-12-27 15:55:25 +0300

    Extend error response.

--- src/validatePayload.ts
+++ src/validatePayload.ts
@@ -1,11 +1,13 @@
-import { CodedError, CommonErrorCodes } from '@vladbasin/strong-api-models';
+import { CodedError, CommonErrorCodes, InnerErrorType } from '@vladbasin/strong-api-models';
 import { ObjectSchema } from 'joi';
 
 export const validatePayload = <T>(payload: T, schema: ObjectSchema<T>) => {
     const { error } = schema.validate(payload, { allowUnknown: true });
 
     if (error) {
-        throw CodedError.from(error, CommonErrorCodes.validationFailed);
+        const innerErrors: InnerErrorType[] = error.details.map(detail => ({ code: detail.context?.key || '', message: detail.type }));
+
+        throw CodedError.from(error, CommonErrorCodes.validationFailed, innerErrors);
     }
 
     return payload;

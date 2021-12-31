b4c034b4c04d15b7766469d73f4f5a3c1e191a22
Vlad Basin <vladbasin@gmail.com>
2021-12-31 03:15:44 +0300

    Allow undefined payload.

--- src/getMappingMetadata.ts
+++ src/getMappingMetadata.ts
@@ -1,6 +1,7 @@
+import { isNil } from 'lodash';
 import { MappingMetadataType } from './types';
 import { MappingMetadataKey } from './models';
 
 export const getMappingMetadata = (target: any) => {
-    return (Reflect.getMetadata(MappingMetadataKey, target) as MappingMetadataType[]) || [];
+    return isNil(target) ? [] : (Reflect.getMetadata(MappingMetadataKey, target) as MappingMetadataType[]) || [];
 };
